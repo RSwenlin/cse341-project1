45cf2ec4267765a1d006f05d53ba97027ca959d9
Robyn <Robyn796@mac.com>
2024-11-05 11:37:38 -0700

    url update

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-const router = require('express');
+const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users');
 
 router.get('/', usersController.getAll);
-
 router.get('/:id', usersController.getSingle);
 
 module.exports = router;
+
 
 
 
