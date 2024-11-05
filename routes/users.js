034dd465e506cc71b52173debd8d75c8ce22a54b
Robyn <Robyn796@mac.com>
2024-11-05 09:06:30 -0700

    adding user.js in routes and controllers(new folder)

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users');
+
+router.get('/', usersController.getAll);
+
+router.get('/:id', usersController.getSingle);
+
+
