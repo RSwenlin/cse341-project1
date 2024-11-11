e2d6585ec5d2caecfb7268ab0110ca1f4110d764
Robyn <Robyn796@mac.com>
2024-11-11 16:04:46 -0700

    swagger.js/routes

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 3000;
 //app.use(express.json());
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*' );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-Width, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+
+});
 app.use('/', require('./routes'));
 
 
