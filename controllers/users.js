4dbd3a936413be60895844daff918d6c9308a434
Robyn <Robyn796@mac.com>
2024-11-11 15:19:51 -0700

    final update

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId });
+    const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
@@ -20,31 +20,34 @@ const getSingle = async (req, res) => {
 
 };
 const createUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
     const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
+       // firstName: req.body.firstName,
+       // lastName: req.body.lastName,
+       // email: req.body.email,
+       // favoriteColor: req.body.favoriteColor,
+       // birthday: req.body.birthday
         email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
-    };
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-    if (response.acknowledged > 0) {
+        username: req.body.username,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
+        };
+        const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+        if (response.acknowledged > 0) {
         res.status(204).send();
-    } else {
+        } else {
         res.status(500).json(response.error || 'Some error occurred while updating the user.');
-    }
+        }
 };
 
 const updateUser = async(req, res) => {
     const userId = new ObjectId(req.params.id);
     const user = {
-        username: req.body.username,
         email: req.body.email,
+        username: req.body.username,
         name: req.body.name,
         ipaddress: req.body.ipaddress
     };
-    const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId }, user);
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
     if (response.modifiedCount > 0) {
         res.status(204).send();
     } else {
@@ -54,7 +57,7 @@ const updateUser = async(req, res) => {
 
 const deleteUser = async(req, res) => {
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('users').remove({_id: userId }, true);
+    const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId });
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
+//app.use(express.json());
 
 app.use(bodyParser.json());
 app.use('/', require('./routes'));
